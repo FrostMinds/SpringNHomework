fe5ec7c1a97d7a55352d7ef5937fd78e59c03caa
FrostMind <Priimak98@Gmail.com>
2022-02-17 15:03:17 +1000

    Env_Homework_16

--- src/main/java/skypro/com/springhomework/IntegerListImpl.java
+++ src/main/java/skypro/com/springhomework/IntegerListImpl.java
@@ -191,7 +191,7 @@ public class IntegerListImpl implements IntegerList {
     }
 
     private void sort() {
-        sortInsertion();
+        quickSort(data,0,size-1);
     }
 
     private Integer binarySearch(Integer element) {
@@ -213,6 +213,30 @@ public class IntegerListImpl implements IntegerList {
         }
         return - 1;
     }
+
+    public static void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swapElements(arr, i, j);
+            }
+        }
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+
 
     @Override
     public String toString() {
